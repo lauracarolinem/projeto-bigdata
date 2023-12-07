3cd24eb8ec02ac2e7859fa3adcf2d09d965d00b3
Laura <llauraccarolinemmenezes@gmail.com>
2023-12-07 11:28:44 -0300

    Deu certo

--- app.py
+++ app.py
@@ -20,8 +20,31 @@ def pegar_input ( ):
 
 def mostrar_filmes(genero):
     filmes = colecao.find({"genres": genero})
-    for index, filme in enumerate(filmes):
-        put_text(index, ' - ', filme['title'])
+   
+    for filme in filmes:
+        titulos = filme['title']
+        put_buttons([titulos], onclick=lambda titulos=titulos: detalhes_filme(titulos))
+        
+
+
+
+def detalhes_filme(filme):
+    clear()
+    resultado = colecao.find_one({'title': filme})
+    # nome = resultado.get('name', 'Nome não disponível')
+    nome = resultado.get('title', 'Nome não disponível')
+    generos = resultado.get('genres', [])
+    diretores = resultado.get('directors', [])
+    ano = resultado.get('year', 'Ano não disponível')
+    poster = resultado.get('poster', 'Poster não disponível')
+
+    
+    put_text(f'Nome: {nome}')
+    put_text(f'Ano: {ano}')
+    put_text(f'Generos: {generos}')
+    put_text(f'Diretores: {diretores}')
+    put_image(poster)
+
 
 if __name__ == '__main__':
     start_server(main, port=8080, auto_open_webbrowser=True)
